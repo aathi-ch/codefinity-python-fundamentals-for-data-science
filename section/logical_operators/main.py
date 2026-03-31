94fa79c1c1ed3a1d20e869990ff0c6582a35a28b
aathi-ch <>
2026-03-31 09:38:04 +0000

    Logical Operators

---
+++ section/logical_operators/main.py
@@ -0,0 +1,8 @@
+# The item's discount and stock status have been defined
+discounted = False
+lowStock = True
+
+movingProduct = lowStock==True or discounted==True
+promotion = not lowStock and not discounted
+
+print(f"Is the item eligible for promotion? {promotion}")
